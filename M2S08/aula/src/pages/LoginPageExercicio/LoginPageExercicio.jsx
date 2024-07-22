568d239851c56bd842cb0f73dd9eb23682d424c2
= <=>
2024-07-22 11:59:20 -0300

    Fix: layout login page

--- M2S08/aula/src/pages/LoginPageExercicio/LoginPageExercicio.jsx
+++ M2S08/aula/src/pages/LoginPageExercicio/LoginPageExercicio.jsx
@@ -20,16 +20,18 @@ function LoginPageExercicio() {
 
     return (
         <>
-        <div className='login'>
-            <div className='login-img'>
-                <img src='https://ze.delivery/_next/image?url=https%3A%2F%2Fcourier-images-web.imgix.net%2Fstatic%2Fimg%2Flogo.png%3Fauto%3Dcompress%2Cformat%26fit%3Dmax%26w%3Dundefined%26h%3Dundefined%26dpr%3D2%26fm%3Dpng&w=256&q=75' alt='logo Zé Delivery' />
+        <div className='login-container'>
+            <div className='login'>
+                <div className='login-img'>
+                    <img src='https://ze.delivery/_next/image?url=https%3A%2F%2Fcourier-images-web.imgix.net%2Fstatic%2Fimg%2Flogo.png%3Fauto%3Dcompress%2Cformat%26fit%3Dmax%26w%3Dundefined%26h%3Dundefined%26dpr%3D2%26fm%3Dpng&w=256&q=75' alt='logo Zé Delivery' />
+                </div>
+                <p>Insira seu e-mail para entrar ou se cadastrar:</p>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <input type='text' placeholder='E-mail' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type='password' placeholder='Senha' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type='submit'>Continuar com e-mail</button>
+                </form>
             </div>
-            <p>Insira seu e-mail para entrar ou se cadastrar:</p>
-            <form className='login-form' onSubmit={handleSubmit}>
-                <input type='text' placeholder='E-mail' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type='password' placeholder='Senha' name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type='submit'>Continuar com e-mail</button>
-            </form>
         </div>
         </>
     )
